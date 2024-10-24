e8fbf58101c101d547a844141b456af5148337fa
nico <maimu.kohagura@claves.co.jp>
2024-10-24 10:04:28 +0900

    passwordを最小８文字に変更

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,7 +4,7 @@ class UserTest < ActiveSupport::TestCase
 
   def setup
     @user = User.new(name: "Example User", email: "user@example.com",
-                     password: "foobar", password_confirmation: "foobar")
+                     password: "aaaaaaaa", password_confirmation: "aaaaaaaa")
   end
 
   test "should be valid" do
@@ -68,7 +68,7 @@ class UserTest < ActiveSupport::TestCase
   end
   
   test "password should have a minimum length" do
-    @user.password = @user.password_confirmation = "a" * 5
+    @user.password = @user.password_confirmation = "a" * 7
     assert_not @user.valid?
   end
 end
