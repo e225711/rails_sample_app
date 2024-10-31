5b5b6cf75983f428909b664575abe9313591b155
nico <maimu.kohagura@claves.co.jp>
2024-10-31 11:45:49 +0900

    パスワードが間違えているときのテストを追加

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -6,6 +6,18 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     @user = users(:michael)
   end
 
+  test "login with valid email/invalid password" do
+    get login_path
+    assert_template 'sessions/new'
+    post login_path, params: { session: { email:  @user.email,
+                                          password: "invalid" } }
+    assert_response :unprocessable_entity
+    assert_template 'sessions/new'
+    assert_not flash.empty?
+    get root_path
+    assert flash.empty?
+  end
+
   test "login with invalid information" do
     get login_path
     assert_template 'sessions/new'
@@ -17,12 +29,12 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert flash.empty?
   end
 
-  test "login with valid email/invalid password" do
-    post login_path, params: { session: { email: @user.email,
-                                          password: "password" } }
+  test "login with valid information" do
+    post login_path, params: { session: { email:    @user.email,
+                                          password: 'password' } }
     assert_redirected_to @user
     follow_redirect!
-    assert_response :success
+    assert_template 'users/show'
     assert_select "a[href=?]", login_path, count: 0
     assert_select "a[href=?]", logout_path
     assert_select "a[href=?]", user_path(@user)
