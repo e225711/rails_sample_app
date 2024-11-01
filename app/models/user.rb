f500c001e9795e1559443a6cb6623795552ab536
nico <maimu.kohagura@claves.co.jp>
2024-11-01 11:50:49 +0900

    永続的セッションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ApplicationRecord
     has_secure_password
     validates :password, presence: true, length: { minimum: 6 }
   
+  
     # 渡された文字列のハッシュ値を返す
     def User.digest(string)
       cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
@@ -25,6 +26,11 @@ class User < ApplicationRecord
     def remember
       self.remember_token = User.new_token
       update_attribute(:remember_digest, User.digest(remember_token))
+    end
+  
+    # 渡されたトークンがダイジェストと一致したらtrueを返す
+    def authenticated?(remember_token)
+      BCrypt::Password.new(remember_digest).is_password?(remember_token)
     end
   end
   
