86d3942acd545fd48842da9be8806b967781e63e
Maimu Kohagura <117128646+e225711@users.noreply.github.com>
2024-11-20 14:34:00 +0900

    いらない部分を削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,13 +11,4 @@ class ApplicationController < ActionController::Base
         redirect_to login_url, status: :see_other
       end
     end
-
-    # ログイン済みユーザーかどうか確認
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url, status: :see_other
-      end
-    end
 end
