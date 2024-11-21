15fd249749e8e54eb1255d11d17687208ae2ba4b
Maimu Kohagura <117128646+e225711@users.noreply.github.com>
2024-11-21 09:48:21 +0900

    基本的な画像アップロード

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -70,3 +70,19 @@ class MicropostSidebarTest < MicropostsInterface
     assert_match "#{@user.microposts.count}", response.body
   end
 end
+
+class ImageUploadTest < MicropostsInterface
+
+  test "should have a file input field for images" do
+    get root_path
+    assert_select 'input[type=file][name="micropost[image]"]'
+  end
+
+  test "should be able to attach an image" do
+    cont = "This micropost really ties the room together."
+    img  = fixture_file_upload('kitten.jpg', 'image/jpeg')
+    post microposts_path, params: { micropost: { content: cont, image: img } }
+    micropost = assigns(:micropost)
+    assert micropost.image.attached?
+  end
+end
